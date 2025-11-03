c3fbbfa54b4a44fd5c03c42e11bdcf370012c7b3
mario32111 <marioge44@gmail.com>
2025-11-03 14:38:37 -0600

    feat: webhook twilio

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use(express.urlencoded({ extended: true }));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const { VoiceResponse } = require('twilio').twiml;
+// 1. Importa el generador de TwiML
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+
+
+
+router.post('/voice', (request, response) => {
+  
+  // 1. (Opcional pero recomendado) Imprime el body para ver qué info te llega
+  console.log("Datos de la llamada:", request.body);
+  const city = request.body.FromCity || "tu ubicación"; // Pon un fallback
+
+  // 2. Prepara la respuesta TwiML
+  const twiml = new VoiceResponse();
+
+  // 3. Aquí está el cambio:
+  twiml.say({
+      voice: 'es-MX-Standard-A', // Voz femenina de español (México)
+      language: 'es-MX'
+  }, `Hola. Te has comunicado con el asistente virtual de emergencias de ${city}. Por favor, describe tu situación.`);
+
+  // 4. (Quitamos el .play() de la canción, ya no es necesario)
+
+  // 5. Envía la respuesta
+  response.type('text/xml');
+  response.send(twiml.toString());
 });
 
 module.exports = router;
